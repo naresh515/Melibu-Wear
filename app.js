5027049ba0be13989894922093a7f60dfe0be420
Naresh Tak <taknaresh515@gmail.com>
2023-04-11 16:59:27 +0530

    slick slider work in popu

--- app.js
+++ app.js
@@ -8,34 +8,49 @@ $(document).ready(function () {
         dots: true,
         autoplay: true,
         autoplaySpeed: 5000,
-    })
-
-    $(".next-btn").click(function () {
-        $(".carousel").slick("slickNext");
     });
 
-    $('.popup').on('click', function () {
-        $('.img-overlay').addClass('img-overlay-open');
-        var $clone = $(this).clone();
-        $('.zoom-img').empty().append($clone);
-        $('body').addClass('removeScroll');
+    $(".next-btn").click(function () {
+        $('.carousel').slick("slickNext");
+    });
 
-        setTimeout(function () {
-            $clone.find('.carousel').slick('unslick');
-            $clone.find('.carousel').slick({
-                slidesToScroll: 1,
-                infinite: false,
-                slidesToShow: 4,
-                dots: true,
-                autoplay: true,
-                autoplaySpeed: 5000
-            });
-        }, 500);
+    $('.popup').click(function () {
+        $('.carousel').slick('unslick');
+
+        $(".img-overlay").addClass("img-overlay-open");
+        $(this).clone().appendTo(".zoom-img");
+        $("body").addClass("removeScroll");
+
+        $('.zoom-img .carousel').slick({
+            variableWidth: true,
+            arrows: false,
+            slidesToScroll: 1,
+            infinite: true,
+            slidesToShow: 1,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+        });
+        
+        $(".next-btn").click(function () {
+            $('.carousel').slick("slickNext");
+        });
     });
 
     $(".close-btn").click(function () {
         $(".img-overlay").removeClass("img-overlay-open");
         $(".zoom-img").empty();
-        $("body").removeClass("removeScroll")
+        $("body").removeClass("removeScroll");
+
+        $('.carousel').slick({
+            variableWidth: true,
+            arrows: false,
+            slidesToScroll: 1,
+            infinite: true,
+            slidesToShow: 1,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+        });
     });
-})
+});
